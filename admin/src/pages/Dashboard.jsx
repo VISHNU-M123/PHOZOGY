e3bfbaff1fbf095d56d505647da3cacfbf03febe
vishnu <vishnuthambankv@gmail.com>
2025-06-07 22:36:17 +0530

    dashboard table start

--- admin/src/pages/Dashboard.jsx
+++ admin/src/pages/Dashboard.jsx
@@ -8,6 +8,9 @@ import { BiSolidCloudDownload } from "react-icons/bi";
 import { GoClockFill } from "react-icons/go";
 import { HiOutlineMailOpen } from "react-icons/hi";
 import { MdPieChart } from "react-icons/md";
+import { IoLogoCodepen } from "react-icons/io";
+import { MdCardTravel } from "react-icons/md";
+import { FiMonitor } from "react-icons/fi";
 
 const Dashboard = () => {
 
@@ -112,8 +115,8 @@ const Dashboard = () => {
               </div>
 
               {/* second div */}
-              <div className="flex flex-col md:flex-row md:gap-6">
-                <div className="mb-[24px] md:w-4/12 flex items-stretch">
+              <div className="flex flex-col md:flex-row -mx-3">
+                <div className="mb-[24px] md:w-4/12 flex items-stretch px-3">
                   <div className="bg-[#191c24] rounded-[4px] w-full">
                     <div className="py-[28px] px-[25px]">
                       <h1 className="text-white mb-[18px] text-[18px] font-[500]">Transaction History</h1>
@@ -123,7 +126,7 @@ const Dashboard = () => {
                         </div>
                         <div className="absolute left-0 right-0 top-0 bottom-0 m-auto w-[100px] h-[20px] text-center text-[18px] text-white">$1200 <span className="text-[#6c7293] text-[12px] text-center block">Total</span></div>
                       </div>
-                      <div className="px-[24px] flex md:flex-col xl:flex-row py-[16px] mt-[16px]">
+                      <div className="xl:px-[24px] md:px-[16px] px-[24px] flex md:flex-col xl:flex-row py-[16px] mt-[16px]">
                         <div className="md:text-center">
                           <h1 className="mb-[4px] text-[15px] font-[500] text-white">Transfer to Paypal</h1>
                           <p className="text-[#6c7293] text-[14px]">07 Jan 2019, 09:12AM</p>
@@ -132,7 +135,7 @@ const Dashboard = () => {
                           <h1 className="text-[15px] font-[700] text-white">$236</h1>
                         </div>
                       </div>
-                      <div className="px-[24px] flex md:flex-col xl:flex-row py-[16px] mt-[16px]">
+                      <div className="xl:px-[24px] md:px-[16px] px-[24px] flex md:flex-col xl:flex-row py-[16px] mt-[16px]">
                         <div className="md:text-center">
                           <h1 className="mb-[4px] text-[15px] font-[500] text-white">Transfer to Stripe</h1>
                           <p className="text-[#6c7293] text-[14px]">07 Jan 2019, 09:12AM</p>
@@ -144,7 +147,7 @@ const Dashboard = () => {
                     </div>
                   </div>
                 </div>
-                <div className="md:w-8/12 mb-[24px] flex items-stretch">
+                <div className="md:w-8/12 mb-[24px] flex items-stretch px-3">
                   <div className="w-full rounded-[4px] bg-[#191c24]">
                     <div className="py-[28px] px-[25px]">
                       <div className="flex items-center justify-between">
@@ -241,6 +244,127 @@ const Dashboard = () => {
                             </div>
                           </div>
                         </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/* third div */}
+              <div className="flex flex-col md:flex-row -mx-3">
+                <div className="md:w-4/12 w-full mb-[24px] px-3">
+                  <div className="bg-[#191c24] rounded-[4px]">
+                    <div className="py-[28px] px-[25px]">
+                      <h1 className="text-[16px] font-[500] mb-[8px] text-white">Revenue</h1>
+                      <div className="flex items-center md:flex-col lg:flex-col xl:flex-row flex-row justify-between">
+                        <div className="my-auto xl:w-8/12 sm:w-full">
+                          <div className="flex items-center">
+                            <h1 className="text-[30px] font-[500] text-white">$32123</h1>
+                            <p className="text-[14px] font-[500] text-green-500 ml-[8px]">+3.5%</p>
+                          </div>
+                          <h1 className="text-[#6c7293] text-[15px] mb-[8px]">11.38% Since last month</h1>
+                        </div>
+                        <div className="xl:w-4/12 sm:w-full text-center flex justify-center">
+                          <IoLogoCodepen size={60} className="text-blue-500" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="md:w-4/12 w-full mb-[24px] px-3">
+                  <div className="bg-[#191c24] rounded-[4px]">
+                    <div className="py-[28px] px-[25px]">
+                      <h1 className="text-[16px] font-[500] mb-[8px] text-white">Sales</h1>
+                      <div className="flex items-center md:flex-col lg:flex-col xl:flex-row flex-row justify-between">
+                        <div className="my-auto xl:w-8/12 sm:w-full">
+                          <div className="flex items-center">
+                            <h1 className="text-[30px] font-[500] text-white">$45850</h1>
+                            <p className="text-[14px] font-[500] text-green-500 ml-[8px]">+8.3%</p>
+                          </div>
+                          <h1 className="text-[#6c7293] text-[15px] mb-[8px]">9.61% Since last month</h1>
+                        </div>
+                        <div className="xl:w-4/12 sm:w-full text-center flex justify-center">
+                          <MdCardTravel size={60} className="text-red-500" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="md:w-4/12 w-full mb-[24px] px-3">
+                  <div className="bg-[#191c24] rounded-[4px]">
+                    <div className="py-[28px] px-[25px]">
+                      <h1 className="text-[16px] font-[500] mb-[8px] text-white">Purchase</h1>
+                      <div className="flex items-center md:flex-col lg:flex-col xl:flex-row flex-row justify-between">
+                        <div className="my-auto xl:w-8/12 sm:w-full">
+                          <div className="flex items-center">
+                            <h1 className="text-[30px] font-[500] text-white">$2039</h1>
+                            <p className="text-[14px] font-[500] text-red-500 ml-[8px]">-2.1%</p>
+                          </div>
+                          <h1 className="text-[#6c7293] text-[15px] mb-[8px]">2.27% Since last month</h1>
+                        </div>
+                        <div className="xl:w-4/12 sm:w-full text-center flex justify-center">
+                          <FiMonitor size={60} className="text-green-500" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/* fourth div */}
+              <div>
+                <div>
+                  <div>
+                    <div>
+                      <h1>Order Status</h1>
+                      <div>
+                        <table>
+                          <thead>
+                            <tr>
+                              <th>
+                                <div>
+                                  <label htmlFor="">
+                                    <input type="text" />
+                                  </label>
+                                </div>
+                              </th>
+                              <th>Client Name</th>
+                              <th>Order No</th>
+                              <th>Product Cost</th>
+                              <th>Project</th>
+                              <th>Payment Mode</th>
+                              <th>Start Date</th>
+                              <th>Payment Status</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td>
+                                <div>
+                                  <label htmlFor="">
+                                    <input type="text" />
+                                  </label>
+                                </div>
+                              </td>
+                              <td>
+                                <img src="" alt="" />
+                                <span>Henry Klein</span>
+                              </td>
+                              <td>02312</td>
+                              <td>$14,500</td>
+                              <td>Dashboard</td>
+                              <td>Credit card</td>
+                              <td>04 Dec 2019</td>
+                              <td>
+                                <div>Approved</div>
+                              </td>
+                            </tr>
+                            <tr></tr>
+                            <tr></tr>
+                            <tr></tr>
+                            <tr></tr>
+                          </tbody>
+                        </table>
                       </div>
                     </div>
                   </div>
